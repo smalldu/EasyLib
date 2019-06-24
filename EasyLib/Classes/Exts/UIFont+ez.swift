f3ed16859d8b162b23c16552c551135ddcd83434
duzhe <363958265@qq.com>
2019-06-24 10:50:20 +0800

    update

--- EasyLib/Classes/Exts/UIFont+ez.swift
+++ EasyLib/Classes/Exts/UIFont+ez.swift
@@ -10,35 +10,43 @@ import UIKit
 
 public extension UIFont {
   
-  static func lightOf(_ size:CGFloat) -> UIFont {
-    if let font = UIFont(name: "PingFangSC-Light" , size: size){
+  static func translateSize(_ size: CGFloat) -> CGFloat {
+    var newSize = size
+    if UIDevice.isiPhone5() {
+      newSize = size - 1
+    }
+    return newSize
+  }
+  
+  static func lightOf(_ size: CGFloat) -> UIFont {
+    if let font = UIFont(name: "PingFangSC-Light" , size: translateSize(size)){
       return font
     }else{
-      return UIFont.systemFont(ofSize: size)
+      return UIFont.systemFont(ofSize: translateSize(size))
     }
   }
   
-  static func regularOf(_ size:CGFloat) -> UIFont {
-    if let font = UIFont(name: "PingFangSC-Regular" , size: size){
+  static func regularOf(_ size: CGFloat) -> UIFont {
+    if let font = UIFont(name: "PingFangSC-Regular" , size: translateSize(size)){
       return font
     }else{
-      return UIFont.systemFont(ofSize:size)
+      return UIFont.systemFont(ofSize:translateSize(size))
     }
   }
   
-  static func mediumOf(_ size:CGFloat)  -> UIFont {
-    if let font = UIFont(name: "PingFangSC-Medium" , size: size){
+  static func mediumOf(_ size: CGFloat)  -> UIFont {
+    if let font = UIFont(name: "PingFangSC-Medium" , size: translateSize(size)){
       return font
     }else{
-      return UIFont.boldSystemFont(ofSize: size)
+      return UIFont.boldSystemFont(ofSize: translateSize(size))
     }
   }
   
-  static func semiboldOf(_ size:CGFloat)  -> UIFont {
-    if let font = UIFont(name: "PingFangSC-Semibold" , size: size){
+  static func semiboldOf(_ size: CGFloat)  -> UIFont {
+    if let font = UIFont(name: "PingFangSC-Semibold" , size: translateSize(size)){
       return font
     }else{
-      return UIFont.boldSystemFont(ofSize: size)
+      return UIFont.boldSystemFont(ofSize: translateSize(size))
     }
   }
   
